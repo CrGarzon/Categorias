68d4f8d553b23bc8f200f980808bc511632497ce
Cristian garzon <cristian.garzonor@cun.edu.co>
2022-02-07 18:49:55 -0500

    create entitie

---
+++ src/main/java/com/categorias/categoria/entities/Categoria.java
@@ -0,0 +1,11 @@
+package com.categorias.categoria.entities;
+
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+
+@EntityScan
+@Table(name = "CATEGORIA")
+
+public class Categoria {
+
+}
