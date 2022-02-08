f845ac1141f8fc0ca70ea389d6bd8e2df7b477f0
Cristian garzon <cristian.garzonor@cun.edu.co>
2022-02-08 14:32:10 -0500

    Add new dependecy and create entitie

--- src/main/java/com/categorias/categoria/CategoriaApplication.java
+++ src/main/java/com/categorias/categoria/CategoriaApplication.java
@@ -5,13 +5,14 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 
 @SpringBootApplication
 @EnableAutoConfiguration
 @EntityScan(basePackages = {"com.categorias.categoria.entities"})
 @ComponentScan(basePackages = "com.categorias.categoria,com.categorias.categoria.entities,com.categorias.categoria.repositories,com.categorias.categoria.controller")
-
+@EnableJpaRepositories
 
 public class CategoriaApplication {
 
--- src/main/java/com/categorias/categoria/entities/Categoria.java
+++ src/main/java/com/categorias/categoria/entities/Categoria.java
@@ -1,5 +1,10 @@
 package com.categorias.categoria.entities;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 
@@ -8,4 +13,30 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 public class Categoria {
 
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	private String nombre;
+	private String descripcion;
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getDescripcion() {
+		return descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	
 }
