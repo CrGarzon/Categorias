ba13a0cbfd9a50d8b70ce4701ead87dc230d6e8e
Cristian garzon <cristian.garzonor@cun.edu.co>
2022-02-08 14:37:56 -0500

    create repository

---
+++ src/main/java/com/categorias/categoria/repositories/CategoriaRepository.java
@@ -0,0 +1,13 @@
+package com.categorias.categoria.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.categorias.categoria.entities.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+	List<Categoria> findByName (String name);
+}
